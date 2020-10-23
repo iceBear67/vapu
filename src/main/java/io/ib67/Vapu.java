e41ef364a243afbdd74b722479c3bf0dec4f9a2e
iceBear67 <icebear67@sfclub.cc>
2020-10-23 22:17:31 +0800

    临时同步代码

--- src/main/java/io/ib67/Vapu.java
+++ src/main/java/io/ib67/Vapu.java
@@ -4,16 +4,21 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import a.a;
 
 @Mod(modid = Vapu.MODID, version = Vapu.VERSION)
 public class Vapu
 {
     public static final String MODID = "vapu";
     public static final String VERSION = "1.0";
+    public static final Logger logger= LogManager.getLogger("VAPE");
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-
+        logger.info("Starting Vape.");
+        a.start();
     }
 }
--- src/main/java/io/ib67/mixins/Mixin_a_a.java
+++ src/main/java/io/ib67/mixins/Mixin_a_a.java
@@ -1,4 +1,14 @@
 package io.ib67.mixins;
 
+import org.spongepowered.asm.mixin.Mixin;
+import a.a;
+import org.spongepowered.asm.mixin.Overwrite;
+
+//@Mixin(value = a.class,remap = false)
 public class Mixin_a_a {
+    //@Overwrite
+    @SuppressWarnings("all")
+    public static String cs(int var0){
+        return "OHHHHH";
+    }
 }
